9d16a9c1b8d950d82be8949e48b843c45a578481
jaugustin <jaugustin.pixelcookers@tf1.fr>
2011-12-30 11:15:08 +0100

    add log Sql queries for propel

--- lib/log/sfFirePHPLogger.php
+++ lib/log/sfFirePHPLogger.php
@@ -60,6 +60,7 @@ class sfFirePHPLogger extends sfVarLogger
   {
     $this->sendMemory();
     $this->sendTimers();
+    $this->sendSQL();
     if (sfConfig::get('sf_logging_enabled'))
     {
       $this->sendConfig();
@@ -97,6 +98,24 @@ class sfFirePHPLogger extends sfVarLogger
 
   }
 
+  protected function sendSQL()
+  {
+      if (class_exists('sfWebDebugPanelPropel')) {
+          if ($sqlLogs = $this->getSqlLogs()) {
+              $this->sfFire->group('SQL Queries: '.count($sqlLogs), array('Collapsed' => true));
+              foreach ($sqlLogs as $log) {
+                  if (isset($log['slow']) && true === $log['slow']) {
+                      $this->sfFire->warn($log['query']);
+                      $this->sfFire->log($log['detail']);
+                  } else {
+                      $this->sfFire->info($log['query']);
+                      $this->sfFire->log($log['detail']);
+                  }
+              }
+              $this->sfFire->groupEnd();
+          }
+      }
+  }
 
 
   protected function sendMemory()
@@ -173,4 +192,73 @@ class sfFirePHPLogger extends sfVarLogger
   {
     return isset($_SERVER['REQUEST_TIME']) ? sprintf('%.0f', (microtime(true) - $_SERVER['REQUEST_TIME']) * 1000) : 0;
   }
-}
+
+  protected function getSqlLogs()
+  {
+      $config    = $this->getPropelConfiguration();
+      $outerGlue = $config->getParameter('debugpdo.logging.outerglue', ' | ');
+      $innerGlue = $config->getParameter('debugpdo.logging.innerglue', ': ');
+      $flagSlow  = $config->getParameter('debugpdo.logging.details.slow.enabled', false);
+      $threshold = $config->getParameter('debugpdo.logging.details.slow.threshold', DebugPDO::DEFAULT_SLOW_THRESHOLD);
+
+      $html = array();
+      $event = $this->dispatcher->filter(new sfEvent($this, 'debug.fire_php.filter_logs'), $this->logs);
+      $logs = $event->getReturnValue();
+      foreach ($logs as $log)
+      {
+          if ('sfPropelLogger' != $log['type'])
+          {
+              continue;
+          }
+
+          $details = array();
+          $slowQuery = false;
+
+          $parts = explode($outerGlue, $log['message']);
+          foreach ($parts as $i => $part)
+          {
+              // is this a key-glue-value fragment ?
+              if (preg_match('/^(\w+)'.preg_quote($innerGlue, '/').'(.*)/', $part, $match))
+              {
+                  $details[] = $part;
+                  unset($parts[$i]);
+
+                  // check for slow query
+                  if ('time' == $match[1])
+                  {
+                      if ($flagSlow && (float) $match[2] > $threshold)
+                      {
+                          $slowQuery = true;
+                      }
+                  }
+              }
+          }
+          // all stuff that has not been eaten by the loop should be the query string
+          $query = join($outerGlue, $parts);
+          if ($query == "SET NAMES 'utf8'")
+          {
+              // This is the initialization query that occurs on every request.
+              // It is not significant, so prune it.
+              continue;
+          }
+
+          $html[] = array(
+                  'query'  => $query,
+                  'detail' => implode(', ', $details),
+                  'slow'   => ($slowQuery == true)
+                  );
+      }
+
+      return $html;
+  }
+
+  /**
+   * Returns the current PropelConfiguration.
+   *
+   * @return PropelConfiguration
+   */
+  protected function getPropelConfiguration()
+  {
+      return Propel::getConfiguration(PropelConfiguration::TYPE_OBJECT);
+  }
+
